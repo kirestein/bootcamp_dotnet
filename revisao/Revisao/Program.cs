9e11024c0c02f20309ab489946f3c3a22aadaff2
= <=>
2021-06-17 16:40:04 -0300

    Construindo a aplicação

--- revisao/Revisao/Program.cs
+++ revisao/Revisao/Program.cs
@@ -6,12 +6,9 @@ namespace Revisao
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Informe a informação desejada:");
-            Console.WriteLine("1 - Inserir novo aluno");
-            Console.WriteLine("2 - Listar alunos");
-            Console.WriteLine("3 - Calcular média geral");
-            Console.WriteLine();
+            
             Aluno[] alunos = new Aluno[5];
+            var indiceAluno = 0;
             string opcaoUsuario = Console.ReadLine();
 
             while (opcaoUsuario.ToUpper() != "x");
@@ -30,10 +27,16 @@ namespace Revisao
                         } else {
                             throw new ArgumentException("Valor da nota deve ser decimal.");
                         }
+
+                        alunos[indiceAluno] = aluno;
+                        indiceAluno++;
                         
                         break;
                     case "2":
-                        //todo: listar alunos
+                        foreach (var a in alunos)
+                        {
+                            Console.WriteLine($"Aluno: {a.Nome} - Nota: {a.Nota}");
+                        }
                         break;
                     case "3":
                         //todo: calcular média geral
@@ -42,15 +45,22 @@ namespace Revisao
                     default:
                         throw new ArgumentOutOfRangeException();
                 }
-                Console.WriteLine("Informe a opção desejadda: ");
+                opcaoUsuario = ObterOpcaoUsuario();
+                
+            }
+            
+        }
+        private static string ObterOpcaoUsuario() 
+        {
+            Console.WriteLine();
+        Console.WriteLine("Informe a opção desejadda: ");
                 Console.WriteLine("1 - Inserir novo aluno");
                 Console.WriteLine("2 - Listar alunos");
                 Console.WriteLine("3 - Calcular média geral");
                 Console.WriteLine();
 
-                opcaoUsuario = Console.ReadLine();
-            }
-            
+                string opcaoUsuario = Console.ReadLine();
+                return opcaoUsuario;
         }
     }
 }
